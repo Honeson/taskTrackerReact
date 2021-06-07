0b969f676c94e94644898e216a0dbe4828741b12
Nwovu Sunday <honesty1ofa1i@gmail.com>
2021-06-07 09:45:54 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {useState} from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
 function App() {
+      const [tasks, setTasks] = useState([
+              {
+                  id: 1,
+                  text: 'HCF Admins Meeting',
+                  day: 'Jun 7th at 8:00pm',
+                  reminder: true
+              },
+              {
+                  id: 2,
+                  text: 'CDS Meeting',
+                  day: 'Jun 10th at 8:00am',
+                  reminder: true
+              },
+              {
+                  id: 3,
+                  text: 'Go to church',
+                  day: 'Jun 13th at 7:00am',
+                  reminder: false
+              }
+      ])
+
+      //Function to delete task
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+//Function to toggle Reminder
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      {tasks.length > 0 ? <Tasks tasks = {tasks} 
+      onDelete={deleteTask} onToggle = {toggleReminder} /> : 'No Task to show yet'}
+      
     </div>
   );
 }
+
+
 
 export default App;
